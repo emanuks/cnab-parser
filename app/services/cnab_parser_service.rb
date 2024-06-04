88f7fb668fc1cba569461f46a987b6b8783f28f0
emanuks <emanuels313@gmail.com>
2024-06-03 21:38:16 -0300

    feat: adding format validation to service

--- app/services/cnab_parser_service.rb
+++ app/services/cnab_parser_service.rb
@@ -5,5 +5,14 @@ class CnabParserService
 
   def call
     file = File.open(@file_path)
+    validate_format(file)
+  end
+
+  private
+
+  def validate_format(file)
+    file.each do |line|
+      raise StandardError unless line.length == 81
+    end
   end
 end
