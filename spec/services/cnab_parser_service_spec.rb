a11d801c8c62ca83c141d16bc1c0fbb98a1c76e1
emanuks <emanuels313@gmail.com>
2024-06-03 21:47:58 -0300

    feat: adding fields validation test

--- spec/services/cnab_parser_service_spec.rb
+++ spec/services/cnab_parser_service_spec.rb
@@ -15,5 +15,47 @@ describe CnabParserService do
         expect { service.call }.to raise_error(StandardError)
       end
     end
+
+    context 'with valid file path but with invalid type' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_type.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with valid file path but with invalid date' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_date.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with valid file path but with invalid value' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_value.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with valid file path but with invalid cpf' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_cpf.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with valid file path but with invalid bank card' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_bank_card.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
+
+    context 'with valid file path but with invalid time' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_time.txt')
+        expect { service.call }.to raise_error(StandardError)
+      end
+    end
   end
 end
