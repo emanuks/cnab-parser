3a0ca5b0e31824ddb763dc339ae7d6acb2ecec7e
emanuks <emanuels313@gmail.com>
2024-06-03 21:35:33 -0300

    feat: adding cnab formatting test

--- app/services/cnab_parser_service.rb
+++ app/services/cnab_parser_service.rb
@@ -1,5 +1,9 @@
 class CnabParserService
-  def initialize(file_path); end
+  def initialize(file_path)
+    @file_path = file_path
+  end
 
-  def call; end
+  def call
+    file = File.open(@file_path)
+  end
 end
--- spec/services/cnab_parser_service_spec.rb
+++ spec/services/cnab_parser_service_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 describe CnabParserService do
   describe '.call' do
     context 'with invalid file path' do
-      it 'throws an error' do
+      it 'raise an exception' do
         service = described_class.new('invalid_file_path.txt')
-        expect { service.call }.to raise_error(ArgumentError)
+        expect { service.call }.to raise_error(Errno::ENOENT)
+      end
+    end
+
+    context 'with valid file path but with incorrect amount of characters per line' do
+      it 'raise an exception' do
+        service = described_class.new('incorrect_CNAB.txt')
+        expect { service.call }.to raise_error(StandardError)
       end
     end
   end
